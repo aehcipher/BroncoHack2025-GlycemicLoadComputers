fca59a116c579c4baf1fced699a1ca470cb49dd6
aehcipher <aehcipher@gmail.com>
2025-04-19 00:51:05 -0700

    Added Credits Page
    Added a credits page that can be accessed from main menu, added a Back button to return to main menu.

--- main_menu.gd
+++ main_menu.gd
@@ -8,9 +8,7 @@ func _on_start_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	print("Credits pressed!")
-	# the scene that occcurs when we press start will be
-	# implemented by using this: get_tree().change_scene_to_file("path to next scene")
+	get_tree().change_scene_to_file("res://Credits.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
