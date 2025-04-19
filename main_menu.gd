83f58704e94aa675aabd8e6c65ea1f62e8b18b4a
aehcipher <aehcipher@gmail.com>
2025-04-18 23:44:29 -0700

    Made main_menu
    Added background, game title, and three clickable buttons.

---
+++ main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	print("Start pressed!")
+	# the scene that occcurs when we press start will be
+	# implemented by using this: get_tree().change_scene_to_file("path to next scene")
+
+
+func _on_credits_pressed() -> void:
+	print("Credits pressed!")
+	# the scene that occcurs when we press start will be
+	# implemented by using this: get_tree().change_scene_to_file("path to next scene")
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
