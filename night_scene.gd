a2f3e3d490f51b16d8bf1cc5f8fbcac15858cf86
aehcipher <aehcipher@gmail.com>
2025-04-19 03:57:16 -0700

    Made Night Scene
    Finished most of the night scene, just need to do a bit more stuff.

---
+++ night_scene.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_PayMortgage_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_payFood_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_payOther_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_SignOff_pressed() -> void:
+	pass # Replace with function body.
